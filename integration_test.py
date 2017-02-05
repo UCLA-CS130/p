11e2529a4bbe6e34417398ed5930ab68817a79e1
Shuang Wang <shuangwang@Shuangs-MacBook-Pro.local>
2017-02-04 21:46:50 -0800

    Add to integration test.

--- integration_test.py
+++ integration_test.py
@@ -3,6 +3,7 @@ import subprocess
 print('Starting webserver...')
 webserver = subprocess.Popen(['./webserver', 'config'], stdout=subprocess.DEVNULL)
 
+print('TEST 1: Echoing request')
 print('Sending request...')
 curl_proc = subprocess.Popen("curl -s localhost:8080", stdout=subprocess.PIPE, shell=True)
 
@@ -10,10 +11,26 @@ response = curl_proc.stdout.read().decode('utf-8')
 expected = 'GET / HTTP/1.1\r\nUser-Agent: curl/7.35.0\r\nHost: localhost:8080\r\nAccept: */*\r\n\r\n';
 
 print('Checking response...')
-if (response != expected):
+if response != expected:
 	print('FAILED: Response does not match expected!')
 	print('Expected: ' + expected)
 	print('Response: ' + response)
+	webserver.kill()
+	exit(1)
+else:
+	print('SUCCESS!')
+
+print('TEST 2: Serving static files')
+print('Sending request...')
+curl_proc = subprocess.Popen(["curl", "-s", "localhost:8080/index.html", "-o", "output"])
+
+diff_proc = subprocess.Popen(["diff", "-u", "static/index.html", "output"], stdout=subprocess.PIPE)
+diff = diff_proc.stdout.read().decode('utf-8')
+
+print('Checking response...')
+if diff != "":
+	print('FAILED: Response does not match expected!')
+	print(diff)
 	webserver.kill()
 	exit(1)
 else:
