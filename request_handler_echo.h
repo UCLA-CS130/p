4ab74bcab5dbdae2007ba75d123b6f72c57beeee
haoxiangzhx <haoxiangzhx@gmail.com>
2017-02-06 20:21:14 -0800

    update unit test

--- request_handler_echo.h
+++ request_handler_echo.h
@@ -9,20 +9,7 @@ class RequestHandlerEcho : public RequestHandler {
 public:
     RequestHandlerEcho (shared_ptr<unordered_set<string>> paths) : paths(paths) {}
 
-    void get_response(ostream& response, const Request& request) {
-        stringstream content_stream;
-        content_stream << request.method << " " << request.path << " HTTP/" << request.http_version << "\r\n";
-        map<string, string> ordered(request.headers.begin(), request.headers.end());
-        for(auto rit=ordered.rbegin(); rit!=ordered.rend(); ++rit) {
-            content_stream << rit->first << ": " << rit->second << "\r\n";
-        }
-        content_stream << "\r\n";
-        
-        //find length of content_stream (length received using content_stream.tellp())
-        content_stream.seekp(0, ios::end);
-        
-        response <<  "HTTP/1.1 200 OK\r\nContent-Length: " << content_stream.tellp() << "\r\nContent-Type: text/plain\r\n\r\n" << content_stream.rdbuf();
-    }
+    void get_response(ostream& response, const Request& request);
 
     shared_ptr<unordered_set<string>> paths;
 };
--- webserver_test.cc
+++ webserver_test.cc
@@ -40,7 +40,7 @@ TEST_F(WebserverTest, ParseRequest){
 	EXPECT_EQ(req.headers["Host"], "www.team-:p.org");
 }
 
-TEST(RequestHandlerEcho, GetResponse) {
+TEST(RequestHandlerEchoTest, GetResponse) {
 	istringstream is("GET / HTTP/1.1\r\nHost: www.team-:p.org\r\n\r\n");
 	Request req = WebServer::parse_request(is);
 	auto paths = make_shared<unordered_set<string>>();
@@ -53,6 +53,70 @@ TEST(RequestHandlerEcho, GetResponse) {
 }
 
 
-// TEST(RequestHandlerStatic, GetResponse) {
-	
-// }
+class RequestHandlerStaticTest : public ::testing::Test {
+protected:
+	void Response(istringstream& is){
+		Request req = WebServer::parse_request(is);
+		auto paths = make_shared<unordered_map<string, string>>();
+		RequestHandlerStatic handler(paths);
+		(*(handler.paths))["static"] = "file/path0";	
+	    ostream response(&buf);
+    	handler.get_response(response, req);
+	}
+
+	boost::asio::streambuf buf;
+};
+
+TEST_F(RequestHandlerStaticTest, GetResponse) {
+	istringstream is("GET /static/image0.jpg HTTP/1.1\r\nHost: localhost:8080\r\n\r\n");
+	Response(is);
+    EXPECT_EQ(28025, buf.size());
+}
+
+TEST_F(RequestHandlerStaticTest, RootPath){
+	istringstream is("GET /static HTTP/1.1\r\nHost: localhost:8080\r\n\r\n");
+	Response(is);
+	EXPECT_EQ(247, buf.size());
+}
+
+TEST_F(RequestHandlerStaticTest, HtmlFile){
+	istringstream is("GET /static/test.html HTTP/1.1\r\nHost: localhost:8080\r\n\r\n");
+	Response(is);
+	EXPECT_EQ(228, buf.size());
+}
+
+TEST_F(RequestHandlerStaticTest, TxtFile){
+	istringstream is("GET /static/test.txt HTTP/1.1\r\nHost: localhost:8080\r\n\r\n");
+	Response(is);
+	EXPECT_EQ(81, buf.size());
+}
+
+TEST_F(RequestHandlerStaticTest, AudioFile){
+	istringstream is("GET /static/chengdu.mp3 HTTP/1.1\r\nHost: localhost:8080\r\n\r\n");
+	Response(is);
+	EXPECT_EQ(8668257, buf.size());
+}
+
+TEST_F(RequestHandlerStaticTest, PdfFile){
+	istringstream is("GET /static/hw2.pdf HTTP/1.1\r\nHost: localhost:8080\r\n\r\n");
+	Response(is);
+	EXPECT_EQ(94460, buf.size());
+}
+
+TEST_F(RequestHandlerStaticTest, 404NotFound){
+	istringstream is("GET /static/superbowl.mov HTTP/1.1\r\nHost: localhost:8080\r\n\r\n");
+	Response(is);
+	EXPECT_EQ(90, buf.size());
+}
+
+TEST_F(RequestHandlerStaticTest, NoStaticMapping){
+	istringstream is("GET /static5/superbowl.mov HTTP/1.1\r\nHost: localhost:8080\r\n\r\n");
+	Response(is);
+	EXPECT_EQ(87, buf.size());
+}
+
+TEST_F(RequestHandlerStaticTest, FindLastDot){
+	istringstream is("GET /static/superbowl.2017.mov HTTP/1.1\r\nHost: localhost:8080\r\n\r\n");
+	Response(is);
+	EXPECT_EQ(94, buf.size());
+}
